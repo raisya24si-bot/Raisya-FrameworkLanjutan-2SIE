8f7b33b2619cf14449b70186a6526e9b760c1dd5
falahal23 <falahal24si@mahasiswa.pcr.ac.id>
2026-03-09 15:31:34 +0700

    pertemuan-2

---
+++ src/pertemuan-2/HelloWorld.jsx
@@ -0,0 +1,35 @@
+export default function HelloWorld(){
+
+     const propsUserCard = {
+        nama: "Goku",
+        nim: "999999",
+        tanggal: "2025-01-01"
+    }
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>Selamat Belajar ReactJs</p>
+            <GreetingBinjai/>
+            <UserCard nama="Raisya Revalina" nim="2457301120" tanggal={new Date().toLocaleDateString()}/>
+            <UserCard nama="Agnes Jesisca" nim="2457301120" tanggal={new Date().toLocaleDateString()}/>
+            <UserCard {...propsUserCard}/>
+        </div>
+    )
+}
+
+function GreetingBinjai(){
+    return (
+            <small>Salam dari Binjai</small>           
+    )
+}   
+
+function UserCard(props){
+    return (
+        <div>
+            <hr/>
+            <h3>Nama: {props.nama}</h3>
+            <p>NIM: {props.nim}</p>
+            <p>Tanggal: {props.tanggal}</p>
+        </div>
+    )
+}
